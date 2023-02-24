cd0550823819e7d67b17958ab5e2bbde38ebff7c
jazz3YR <yaroslavryabov@hotmail.com>
2023-02-24 12:42:20 +0200

    add shell cript for monitoring CPU specs

---
+++ cpu-mon.sh
@@ -0,0 +1,5 @@
+# cpu frequency & temperature monitoring via script
+#!/bin/sh
+wget https://gist.githubusercontent.com/PhillipJacobsen/b829bdcd8f345431479ac2aa1f0de8da/raw/e2046285bac6d4b0f95cd65b093d3d17755c479a/cpumonitor.sh
+chmod +x cpumonitor.sh
+./cpumonitor.sh
